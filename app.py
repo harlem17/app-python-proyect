143651db2547349c24bdb511b7a42deebb3af566
Kaleth Alexander Benjumea Hernandez <113316840+kalethabh@users.noreply.github.com>
2023-10-24 01:48:21 +0000

    Se eliminarion los returns

--- app.py
+++ app.py
@@ -19,11 +19,6 @@ async def index(request: Request):
     print('Request for index page received')
     return templates.TemplateResponse('Home.html', {"request": request})
 
-# Ruta para mostrar formulario de registro de voluntario
-#@app.get('/register-voluntario', response_class=HTMLResponse)
-#async def voluntario(request: Request):
- #   return templates.TemplateResponse('FormularioVoluntario.html', {"request": request})
-
 # Ruta para registrar un voluntario
 @app.post('/create-voluntario', response_class=JSONResponse)
 async def add_voluntario(ID: int = Form(...), Nombre: str = Form(...), Apellido: str = Form(...), Telefono: int = Form(...), Intereses: str = Form(...)):
@@ -36,13 +31,7 @@ async def add_voluntario(ID: int = Form(...), Nombre: str = Form(...), Apellido:
     }
 
     voluntarios_db.append(nuevo_voluntario)
-    print('mensaje": "Voluntario agregado con éxito", "nuevo_voluntario": nuevo_voluntario')
-    return{ID:ID,Nombre:Nombre,Apellido:Apellido,Telefono:Telefono,Intereses:Intereses}
-
-# Ruta para eliminar voluntario por ID
-#@app.get('/eliminar-voluntario', response_class=HTMLResponse)
-#async def form_delete_voluntario(request: Request):
- #   return templates.TemplateResponse('FormDeleteVoluntario.html', {"request": request})
+    print('Voluntario agregado con éxito', nuevo_voluntario)
 
 # Ruta para eliminar voluntario por ID
 @app.post('/eliminar-voluntario', response_class=JSONResponse)
@@ -50,18 +39,9 @@ async def delete_voluntario(ID: int = Form(...)):
     for voluntario in voluntarios_db:
         if voluntario['ID'] == ID:
             voluntarios_db.remove(voluntario)
-            return {"mensaje": "Voluntario eliminado con éxito"}
-    return {"mensaje": "Voluntario no encontrado"}
-
-# Ruta para mostrar todos los voluntarios
-@app.get('/voluntarios', response_class=JSONResponse)
-async def mostrar_voluntarios():
-    return {"voluntarios": voluntarios_db}
-
-# Ruta para mostrar formulario de registro de programa
-#@app.get('/register-programa', response_class=HTMLResponse)
-#async def programa(request: Request):
- #   return templates.TemplateResponse('FormularioPrograma.html', {"request": request})
+            print("Voluntario eliminado con éxito")
+            return
+    print("Voluntario no encontrado")
 
 # Ruta para registrar un programa
 @app.post('/create-programa', response_class=JSONResponse)
@@ -73,29 +53,9 @@ async def add_programa(nombre: str = Form(...), descripcion: str = Form(...)):
     }
 
     programas_db.append(nuevo_programa)
-    return {"mensaje": "Programa agregado con éxito", "nuevo_programa": nuevo_programa}
+    print("Programa agregado con éxito", nuevo_programa)
 
-# Ruta para mostrar todos los programas
-@app.get('/programas', response_class=JSONResponse)
-async def mostrar_programas():
-    return {"programas": programas_db}
-
-# Ruta para mostrar formulario de registro en programa
-#@app.get('/asignar-programa', response_class=HTMLResponse)
-#async def AsignarPrograma(request: Request):
- #   return templates.TemplateResponse('AsignarPrograma.html', {"request": request})
-
-# Ruta para que los voluntarios se unan a un programa
-@app.post('/unirse-programa', response_class=JSONResponse)
-async def unirse_programa(nombre_programa: str = Form(...), voluntario_id: int = Form(...)):
-    programa_encontrado = next((programa for programa in programas_db if programa['nombre'] == nombre_programa), None)
-    voluntario_encontrado = next((voluntario for voluntario in voluntarios_db if voluntario['ID'] == voluntario_id), None)
-
-    if programa_encontrado and voluntario_encontrado:
-        programa_encontrado['participantes'].append(voluntario_encontrado)
-        return {"mensaje": "Voluntario agregado al programa con éxito"}
-    else:
-        return {"mensaje": "Programa o voluntario no encontrado"}
+# Resto de tus rutas...
 
 if __name__ == '__main__':
     uvicorn.run('app:app')
