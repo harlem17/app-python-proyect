41cd2a6e6cbfd6eabc1dd10f2de7c89756149cd3
Kaleth Alexander Benjumea Hernandez <113316840+kalethabh@users.noreply.github.com>
2023-10-30 17:49:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ async def add_voluntario(ID: int = Form(...), Nombre: str = Form(...), Apellido:
 
     voluntarios_db.append(nuevo_voluntario)
     print("Voluntario agregado con éxito", nuevo_voluntario)
-    return JSONResponse(content={"mensaje": "Voluntario agregado con éxito", "nuevo_voluntario": nuevo_voluntario})
+    return JSONResponse(content={"mensaje": "Voluntario agregado con éxito", "nuevo_voluntario": nuevo_voluntario}, status_code=200)
 
 # Ruta para eliminar voluntario por ID
 @app.delete('/eliminar-voluntario', response_class=JSONResponse)
@@ -41,9 +41,9 @@ async def delete_voluntario(ID: int = Form(...)):
         if voluntario['ID'] == ID:
             voluntarios_db.remove(voluntario)
             print("Voluntario eliminado con éxito")
-            return JSONResponse(content={"mensaje": "Voluntario eliminado con éxito"})
+            return JSONResponse(content={"mensaje": "Voluntario eliminado con éxito"}, status_code=200)
     print("Voluntario no encontrado")
-    return JSONResponse(content={"mensaje": "Voluntario no encontrado"})
+    return JSONResponse(content={"mensaje": "Voluntario no encontrado"}, status_code=404)
 
 # Ruta para registrar un programa
 @app.post('/create-programa', response_class=JSONResponse)
@@ -56,7 +56,7 @@ async def add_programa(nombre: str = Form(...), descripcion: str = Form(...)):
 
     programas_db.append(nuevo_programa)
     print("Programa agregado con éxito", nuevo_programa)
-    return JSONResponse(content={"mensaje": "Programa agregado con éxito", "nuevo_programa": nuevo_programa})
+    return JSONResponse(content={"mensaje": "Programa agregado con éxito", "nuevo_programa": nuevo_programa}, status_code=200)
 
 # Ruta para que los voluntarios se unan a un programa
 @app.post('/unirse-programa', response_class=JSONResponse)
@@ -67,20 +67,20 @@ async def unirse_programa(nombre_programa: str = Form(...), voluntario_id: int =
     if programa_encontrado and voluntario_encontrado:
         programa_encontrado['participantes'].append(voluntario_encontrado)
         print("Voluntario agregado al programa con éxito")
-        return JSONResponse(content={"mensaje": "Voluntario agregado al programa con éxito"})
+        return JSONResponse(content={"mensaje": "Voluntario agregado al programa con éxito"}, status_code=200)
     else:
         print("Programa o voluntario no encontrado")
-        return JSONResponse(content={"mensaje": "Programa o voluntario no encontrado"})
+        return JSONResponse(content={"mensaje": "Programa o voluntario no encontrado"}, status_code=404)
 
 # Ruta para mostrar todos los voluntarios
 @app.get('/voluntarios', response_class=JSONResponse)
 async def mostrar_voluntarios():
-    return JSONResponse(content={"voluntarios": voluntarios_db})
+    return JSONResponse(content={"voluntarios": voluntarios_db}, status_code=200)
 
 # Ruta para mostrar todos los programas
 @app.get('/programas', response_class=JSONResponse)
 async def mostrar_programas():
-    return JSONResponse(content={"programas": programas_db})
+    return JSONResponse(content={"programas": programas_db}, status_code=200)
 
 if __name__ == '__main__':
     uvicorn.run('app:app')
