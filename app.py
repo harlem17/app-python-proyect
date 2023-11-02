e1fd3694df9b99a8034543a1773d79bd7bca6317
kalethabh <kalethabh@gmail.com>
2023-11-02 13:47:39 -0500

    DB

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from fastapi import FastAPI, Request
-from fastapi import Form
+from fastapi import FastAPI, Request, Form, Depends, HTTPException
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
-import uvicorn
+import sqlite3
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -16,99 +15,97 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Lista para almacenar voluntarios (simulación de una base de datos)
-voluntarios_db = []
+# Conéctate a la base de datos SQLite
+conn = sqlite3.connect('nonprofitorganization.db')
+cursor = conn.cursor()
 
-# Lista para almacenar programas
-programas_db = []
+# Crea una tabla para almacenar voluntarios si no existe
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS voluntarios (
+        id INTEGER PRIMARY KEY,
+        nombre TEXT NOT NULL,
+        apellido TEXT NOT NULL,
+        telefono INTEGER NOT NULL,
+        intereses TEXT
+    )
+''')
+
+# Crea una tabla para almacenar programas si no existe
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS programas (
+        nombre TEXT PRIMARY KEY,
+        descripcion TEXT
+    )
+''')
+
+conn.commit()
+conn.close()
 
 # Ruta para mostrar la página principal
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
-    print('Request for index page received')
     return templates.TemplateResponse('Home.html', {"request": request})
 
 # Ruta para registrar un voluntario
 @app.post('/create-voluntario', response_class=JSONResponse)
 async def add_voluntario(ID: int = Form(...), Nombre: str = Form(...), Apellido: str = Form(...), Telefono: int = Form(...), Intereses: str = Form(...)):
-    nuevo_voluntario = {
-        'ID': ID,
-        'Nombre': Nombre,
-        'Apellido': Apellido,
-        'Telefono': Telefono,
-        'Intereses': Intereses
-    }
-
-    voluntarios_db.append(nuevo_voluntario)
-    print("Voluntario agregado con éxito", nuevo_voluntario)
-    return JSONResponse(content={"mensaje": "Voluntario agregado con éxito", "nuevo_voluntario": nuevo_voluntario}, status_code=200)
+    conn = sqlite3.connect('mydatabase.db')
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO voluntarios (id, nombre, apellido, telefono, intereses) VALUES (?, ?, ?, ?, ?)", (ID, Nombre, Apellido, Telefono, Intereses))
+    conn.commit()
+    conn.close()
+    return JSONResponse(content={"mensaje": "Voluntario agregado con éxito"}, status_code=200)
 
 # Ruta para eliminar voluntario por ID
 @app.delete('/eliminar-voluntario', response_class=JSONResponse)
 async def delete_voluntario(ID: int = Form(...)):
-    for voluntario in voluntarios_db:
-        if voluntario['ID'] == ID:
-            voluntarios_db.remove(voluntario)
-
-            # Eliminar al voluntario de la lista de participantes en los programas
-            for programa in programas_db:
-                if voluntario in programa['participantes']:
-                    programa['participantes'].remove(voluntario)
-
-            print("Voluntario eliminado con éxito")
-            return JSONResponse(content={"mensaje": "Voluntario eliminado con éxito"}, status_code=200)
-
-    print("Voluntario no encontrado")
-    return JSONResponse(content={"mensaje": "Voluntario no encontrado"}, status_code=404)
+    conn = sqlite3.connect('mydatabase.db')
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM voluntarios WHERE id = ?", (ID,))
+    conn.commit()
+    conn.close()
+    return JSONResponse(content={"mensaje": "Voluntario eliminado con éxito"}, status_code=200)
 
 # Ruta para registrar un programa
 @app.post('/create-programa', response_class=JSONResponse)
 async def add_programa(nombre: str = Form(...), descripcion: str = Form(...)):
-    nuevo_programa = {
-        'nombre': nombre,
-        'descripcion': descripcion,
-        'participantes': []
-    }
-
-    programas_db.append(nuevo_programa)
-    print("Programa agregado con éxito", nuevo_programa)
-    return JSONResponse(content={"mensaje": "Programa agregado con éxito", "nuevo_programa": nuevo_programa}, status_code=200)
-
-# Ruta para que los voluntarios se unan a un programa
-@app.post('/unirse-programa', response_class=JSONResponse)
-async def unirse_programa(nombre_programa: str = Form(...), voluntario_id: int = Form(...)):
-    programa_encontrado = next((programa for programa in programas_db if programa['nombre'] == nombre_programa), None)
-    voluntario_encontrado = next((voluntario for voluntario in voluntarios_db if voluntario['ID'] == voluntario_id), None)
-
-    if programa_encontrado and voluntario_encontrado:
-        programa_encontrado['participantes'].append(voluntario_encontrado)
-        print("Voluntario agregado al programa con éxito")
-        return JSONResponse(content={"mensaje": "Voluntario agregado al programa con éxito"}, status_code=200)
-    else:
-        print("Programa o voluntario no encontrado")
-        return JSONResponse(content={"mensaje": "Programa o voluntario no encontrado"}, status_code=404)
+    conn = sqlite3.connect('mydatabase.db')
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO programas (nombre, descripcion) VALUES (?, ?)", (nombre, descripcion))
+    conn.commit()
+    conn.close()
+    return JSONResponse(content={"mensaje": "Programa agregado con éxito"}, status_code=200)
 
 # Ruta para eliminar programa por Nombre
 @app.delete('/eliminar-programa', response_class=JSONResponse)
 async def delete_programa(Nombre: str = Form(...)):
-    for programa in programas_db:
-        if programa['nombre'] == nombre:
-            programas_db.remove(programa)
-            print("Programa eliminado con éxito")
-            return JSONResponse(content={"mensaje": "Programa eliminado con éxito"}, status_code=200)
-
-    print("Programa no encontrado")
-    return JSONResponse(content={"mensaje": "Programa no encontrado"}, status_code=404)
+    conn = sqlite3.connect('mydatabase.db')
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM programas WHERE nombre = ?", (Nombre,))
+    conn.commit()
+    conn.close()
+    return JSONResponse(content={"mensaje": "Programa eliminado con éxito"}, status_code=200)
 
 # Ruta para mostrar todos los voluntarios
 @app.get('/voluntarios', response_class=JSONResponse)
 async def mostrar_voluntarios():
-    return JSONResponse(content={"voluntarios": voluntarios_db}, status_code=200)
+    conn = sqlite3.connect('mydatabase.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM voluntarios")
+    voluntarios = cursor.fetchall()
+    conn.close()
+    return JSONResponse(content={"voluntarios": voluntarios}, status_code=200)
 
 # Ruta para mostrar todos los programas
 @app.get('/programas', response_class=JSONResponse)
 async def mostrar_programas():
-    return JSONResponse(content={"programas": programas_db}, status_code=200)
+    conn = sqlite3.connect('mydatabase.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM programas")
+    programas = cursor.fetchall()
+    conn.close()
+    return JSONResponse(content={"programas": programas}, status_code=200)
 
 if __name__ == '__main__':
-    uvicorn.run('app:app')
+    import uvicorn
+    uvicorn.run('app:app', host='0.0.0.0', port=8000, reload=True)
