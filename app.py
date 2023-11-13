1449cb3a23c6631ce24931c54e0bcbdc79506c51
kalethabh <kalethabh@gmail.com>
2023-11-13 18:06:15 -0500

    .2

--- app.py
+++ app.py
@@ -170,6 +170,8 @@ async def mostrar_voluntarios():
 async def mostrar_programas_con_voluntarios():
     try:
         conn = await get_database_conn()
+
+        # Obtener todos los programas
         query_programas = 'SELECT * FROM programas'
         programas_result = await conn.fetch(query_programas)
 
@@ -177,10 +179,11 @@ async def mostrar_programas_con_voluntarios():
         for programa_row in programas_result:
             programa = {"nombre": programa_row['nombre'], "descripcion": programa_row['descripcion']}
             
-            query_voluntarios = 'SELECT v.nombre, v.apellido FROM voluntarios v INNER JOIN programa_voluntario pv ON v.id = pv.voluntario_id INNER JOIN programas p ON p.nombre = pv.programa_id WHERE p.nombre = $1'
+            # Obtener voluntarios para cada programa
+            query_voluntarios = 'SELECT v.nombre, v.apellido, v.telefono, v.intereses FROM voluntarios v INNER JOIN programa_voluntario pv ON v.id = pv.voluntario_id INNER JOIN programas p ON p.nombre = pv.programa_id WHERE p.nombre = $1'
             voluntarios_result = await conn.fetch(query_voluntarios, programa_row['nombre'])
 
-            voluntarios = [{"nombre": v['nombre'], "apellido": v['apellido']} for v in voluntarios_result]
+            voluntarios = [{"nombre": v['nombre'], "apellido": v['apellido'], "telefono": v['telefono'], "intereses": v['intereses']} for v in voluntarios_result]
             programa["voluntarios"] = voluntarios
 
             programas.append(programa)
